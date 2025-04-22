f577cda4ddd85acf8f1a1be8743c6e2c0738f536
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2025-04-22 15:26:51 +0000

    v1.2

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="../templates",
+    static_folder="../static"
+)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
 @app.route("/static/<path:filename>")
 def static_files(filename):
-    return send_from_directory("../static", filename)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return send_from_directory(app.static_folder, filename)
